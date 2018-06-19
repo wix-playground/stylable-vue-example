26a819a09a272ded872618cec50122a91b446a70
tomr <tomr@wix.com>
2018-06-19 17:07:56 +0300

    Add production mode build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const StylableWebpackPlugin = require('stylable-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 exports.module = {
     rules: [
@@ -7,20 +8,11 @@ exports.module = {
             test: /\.vue$/,
             loader: 'vue-loader'
         },
-        // "file" loader makes sure those assets get served by WebpackDevServer.
-        // When you `import` an asset, you get its (virtual) filename.
-        // In production, they would get copied to the `build` folder.
-        // This loader doesn't use a "test" so it will catch all modules
-        // that fall through the other loaders.
         {
-            // Exclude `js` files to keep "css" loader working as it injects
-            // it's runtime that would otherwise processed through "file" loader.
-            // Also exclude `html` and `json` extensions so they get processed
-            // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/, /\.css$/, /\.vue$/],
-            loader: require.resolve('file-loader'),
+            test: /\.svg$/,
+            loader: 'file-loader',
             options: {
-            name: 'static/media/[name].[hash:8].[ext]',
+                name: 'static/media/[name].[hash:8].[ext]',
             },
         }
     ]
@@ -29,4 +21,5 @@ exports.module = {
 exports.plugins = [
     new StylableWebpackPlugin(),
     new VueLoaderPlugin(),
+    new HtmlWebpackPlugin
 ]
